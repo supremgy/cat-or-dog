c074249d186a4d20b871f5f44f4750e8b1435cc9
supremgy <supremgy@icloud.com>
2024-07-12 00:43:33 +0900

    feat: survey page 개발 1차 완료

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,86 +1,96 @@
+import Header from '@/components/Header';
+import SurveyForm from '@/components/SurveyForm';
 import React from 'react';
 
-const SurveyPage = () => {
+export default function SurveyPage() {
+  //db date fetching
   const survey = {
-    step1: {
+    openness: {
       question: '당신은 새로운 경험이나 아이디어를 받아들이는 것을 즐기시나요?',
       answer: [
         {
-          content: '매우 그렇다',
+          content: '😆 매우 그렇다',
           value: 5,
         },
         {
-          content: '그렇다',
+          content: '😁 그렇다',
           value: 4,
         },
         {
-          content: '보통이다',
+          content: '🙂 보통이다',
           value: 3,
         },
         {
-          content: '아니다',
+          content: '🙁 아니다',
           value: 2,
         },
         {
-          content: '전혀 아니다',
+          content: '🫨 전혀 아니다',
           value: 1,
         },
       ],
     },
-    step2: {
+    neuroticism: {
       question: '당신은 자주 불안하거나 스트레스를 받는 편인가요?',
       answer: [
         {
-          content: '매우 그렇다',
+          content: '🤯 매우 그렇다',
           value: 5,
         },
         {
-          content: '그렇다',
+          content: '😤 그렇다',
           value: 4,
         },
         {
-          content: '보통이다',
+          content: '🙂 보통이다',
           value: 3,
         },
         {
-          content: '아니다',
+          content: '🙂‍↔️ 아니다',
           value: 2,
         },
         {
-          content: '전혀 아니다',
+          content: '😆 전혀 아니다',
           value: 1,
         },
       ],
     },
-    step3: {
+    extroversion: {
       question:
-        '당신의 외향성을 1에서 5까지 점수로 매겨주세요. (1: 매우 내성적, 5: 매우 외향적) (주관식)',
-      answer: undefined,
+        '당신의 외향성을 1(내향적)에서 5(외향적)까지 점수로 매겨주세요.',
     },
-    step4: {
+    agreeableness: {
       question:
         '다음 중 두 가지를 골라주세요. 당신의 성격을 가장 잘 나타내는 것은 무엇인가요?',
       answer: [
         {
-          content: '다른 사람들을 도와주는 것을 좋아합니다',
-          value: 4,
+          content: '👂 친구의 어려움을 경청합니다',
+          value: 1,
         },
         {
-          content: '갈등 상황에서 중재를 잘 합니다',
-          value: 3,
-        },
-        {
-          content: '타인의 감정을 잘 이해합니다',
+          content: '🗣️ 다른 사람의 의견을 존중합니다',
           value: 2,
         },
         {
-          content: '남들과 협력하는 것을 즐깁니다',
-          value: 1,
+          content: '🤝 화합을 위해 노력합니다',
+          value: 3,
+        },
+        {
+          content: '🎉 친구의 기쁨에 함께 기뻐합니다',
+          value: 4,
+        },
+        {
+          content: '⚖️ 모든 사람에게 공정하게 대합니다',
+          value: 5,
         },
       ],
     },
   };
-  return <div className='main-theme'></div>;
-};
 
-export default SurveyPage;
+  return (
+    <div className='main-theme relative'>
+      <Header />
+      <SurveyForm survey={survey} />
+    </div>
+  );
+}
