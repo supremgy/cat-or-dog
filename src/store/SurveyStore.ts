c6b29a49027b7463e558959ae000f6d57ed6905e
supremgy <supremgy@icloud.com>
2024-07-12 00:37:56 +0900

    feat: SurveyStore 생성

--- src/store/SurveyStore.ts
+++ src/store/SurveyStore.ts
@@ -1,15 +1,48 @@
 import { StateCreator } from 'zustand';
 
+export interface StepDataProps {
+  question: string;
+  answer?: Answer[];
+  index?: number[] | number | null;
+  score: number | undefined;
+}
+export interface Answer {
+  content: string;
+  value: number;
+}
 export interface SurveyState {
-  step: string;
-  total: number;
-  setStep: (step: string) => void;
-  addScore: (score: number) => void;
+  registerData: StepDataProps[];
+  setRegisterData: (data: StepDataProps[]) => void;
+  resetRegisterData: () => void;
 }
 
+const defaultRegisterData: StepDataProps[] = [
+  {
+    question: '',
+    answer: [],
+    index: null,
+    score: 0,
+  },
+  {
+    question: '',
+    answer: [],
+    index: null,
+    score: 0,
+  },
+  {
+    question: '',
+    score: 0,
+  },
+  {
+    question: '',
+    answer: [],
+    index: [],
+    score: 0,
+  },
+];
+
 export const createSurveySlice: StateCreator<SurveyState> = (set) => ({
-  step: 'step1',
-  total: 0,
-  setStep: (step: string) => set({ step }),
-  addScore: (score: number) => set((state) => ({ total: state.total + score })),
+  registerData: defaultRegisterData,
+  setRegisterData: (data: StepDataProps[]) => set({ registerData: data }),
+  resetRegisterData: () => set({ registerData: defaultRegisterData }),
 });
