5abcdc02063972beb5c717e23ca45c974d5ecc64
supremgy <supremgy@icloud.com>
2024-07-12 21:38:13 +0900

    refactor: data 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className='main-theme'>
       <Header />
       <section className='flex flex-col items-center mt-36 gap-10'>
-        <div className='text-4xl font-semibold'>Cat🐱? or Dog🐶?</div>
+        <div className='text-3xl font-semibold'>Cat🐱? or Dog🐶?</div>
         <div className='text-xl text-center'>
           고양이인가요? 강아지인가요?
           <br /> 당신의 애완동물 선호도를 알아보세요!
