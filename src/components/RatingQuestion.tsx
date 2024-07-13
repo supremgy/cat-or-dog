ef40f4c26fbbc31478ee92fbfce536c63803a80b
supremgy <supremgy@icloud.com>
2024-07-14 00:16:26 +0900

    refactor: Survey flow contents 수정 및 새로고침시 store data  유지 개발

--- src/components/RatingQuestion.tsx
+++ src/components/RatingQuestion.tsx
@@ -3,14 +3,19 @@ import React, { useState } from 'react';
 import { useStore } from '@/store';
 import { StepProps } from './SingleChoice';
 import ButtonForm from './ButtonForm';
+import { useRouter } from 'next/navigation';
 
-export default function RatingQuestion({ step, setStep }: StepProps) {
-  const registerData = useStore((state) => state.registerData);
-  const setRegisterData = useStore((state) => state.setRegisterData);
+export default function RatingQuestion({
+  step,
+  setStep,
+  survey,
+  sumTotal,
+}: StepProps) {
+  const router = useRouter();
   const setToastState = useStore((state) => state.setToastState);
-  const [score, setScore] = useState<number | undefined>(
-    registerData[step].score as number
-  );
+  const selectedIndex = useStore((state) => state.selectedIndex);
+
+  const [score, setScore] = useState<number | undefined>(undefined);
 
   const handleNext = () => {
     if (score && score > 10) {
@@ -21,48 +26,48 @@ export default function RatingQuestion({ step, setStep }: StepProps) {
       return;
     }
 
-    const newRegisterData = registerData.map((item, i) => {
-      if (i === step) {
-        return {
-          ...item,
-          score,
-        };
-      }
-      return item;
-    });
     if (score) {
-      setRegisterData(newRegisterData);
+      let total = 0;
+      selectedIndex.forEach((item, i) => {
+        if (survey[i].answer) {
+          if (typeof item.index === 'number') {
+            total += survey[i].answer[item.index].value;
+          } else if (Array.isArray(item.index)) {
+            item.index.forEach((index) => {
+              if (survey[i].answer) total += survey[i].answer[index].value;
+            });
+          }
+        }
+      });
+
+      sumTotal(total + score);
+
+      router.push('/result');
     } else {
       setToastState();
       return;
     }
-    setStep(step + 1);
   };
   const handleBack = () => {
-    const newRegisterData = registerData.map((item, i) => {
-      if (i === step) {
-        return {
-          ...item,
-          score,
-        };
-      }
-      return item;
-    });
-    if (score) setRegisterData(newRegisterData);
     setStep(step - 1);
   };
   return (
     <div className='mt-28 w-full'>
-      <div className='mb-10 text-xl font-semibold'>
-        {registerData[step].question}
-      </div>
+      <div
+        className='mb-10 text-xl font-semibold'
+        dangerouslySetInnerHTML={{ __html: survey[step].question }}
+      />
       <input
         type='number'
         className='w-1/5 h-10 text-lg rounded-md p-2 text-teal-600 mb-8 outline-none focus:border-2 focus:border-teal-600 duration-150'
         value={score}
         onChange={(event) => setScore(Number(event.target.value))}
       />
-      <ButtonForm handleBack={handleBack} handleNext={handleNext} />
+      <ButtonForm
+        nextText='완료'
+        handleBack={handleBack}
+        handleNext={handleNext}
+      />
     </div>
   );
 }
--- src/components/SingleChoice.tsx
+++ src/components/SingleChoice.tsx
@@ -1,75 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useStore } from '@/store';
 import ButtonForm from './ButtonForm';
+import Link from 'next/link';
+import { Survey } from '@/app/survey/page';
 
 export interface StepProps {
   step: number;
+  survey: Survey[];
   setStep: (step: number) => void;
+  sumTotal: (score: number) => void;
 }
 
-export default function SingleChoice({ step, setStep }: StepProps) {
-  const registerData = useStore((state) => state.registerData);
-  const setRegisterData = useStore((state) => state.setRegisterData);
+export default function SingleChoice({ step, setStep, survey }: StepProps) {
   const setModalState = useStore((state) => state.setModalState);
   const setToastState = useStore((state) => state.setToastState);
-
+  const selectedIndex = useStore((state) => state.selectedIndex);
+  const setSelectedIndex = useStore((state) => state.setSelectedIndex);
   const [selectedAnswer, setSelectedAnswer] = useState<number | undefined>(
-    registerData[step].index as number
+    selectedIndex[step].index as number
   );
+  useEffect(() => {
+    setSelectedAnswer(selectedIndex[step].index as number);
+  }, [step]);
 
   const handleAnswerClick = (index: number) => {
     setSelectedAnswer(index);
+    setSelectedIndex(index, step);
   };
 
   const handleNext = () => {
     if (typeof selectedAnswer === 'number') {
-      const newRegisterData = registerData.map((item, i) => {
-        if (i === step) {
-          return {
-            ...item,
-            index: selectedAnswer,
-            score: item.answer![selectedAnswer].value,
-          };
-        }
-        return item;
-      });
-      setRegisterData(newRegisterData);
+      setStep(step + 1);
     } else {
       setToastState();
       return;
     }
-    setStep(step + 1);
   };
   const handleBack = () => {
     if (step === 0) {
       setModalState(true);
       return;
     } else {
-      if (selectedAnswer) {
-        const newRegisterData = registerData.map((item, i) => {
-          if (i === step) {
-            return {
-              ...item,
-              index: selectedAnswer,
-              score: item.answer![selectedAnswer].value,
-            };
-          }
-          return item;
-        });
-        setRegisterData(newRegisterData);
-      }
+      setStep(step - 1);
     }
-    setStep(step - 1);
   };
 
   return (
-    <div className='mt-28 w-full'>
-      <div className='mb-10 text-xl font-semibold w-full'>
-        {registerData[step].question}
-      </div>
-      {registerData[step].answer && (
+    <div className='mt-16 size-full'>
+      <div
+        className={`${step === 1 ? 'mb-2' : 'mb-10'} text-xl w-full`}
+        dangerouslySetInnerHTML={{ __html: survey[step].question }}
+      />
+      {step === 1 && (
+        <p className='mb-8'>
+          <span className='mr-2'>👉</span>
+          <Link
+            target='_blank'
+            className='underline'
+            href={
+              'https://do0ori.notion.site/024ab670d29546958b84c2a3c67d9cac?v=844c3ec2b3844e87ac91f30aa515284d'
+            }
+          >
+            (DangDangWalk팀 미팅 노션)
+          </Link>
+        </p>
+      )}
+      {survey[step].answer && (
         <ul className='mb-8  '>
-          {registerData[step].answer!.map((ans, index) => (
+          {survey[step].answer!.map((ans, index) => (
             <li
               key={index}
               onClick={() => handleAnswerClick(index)}
