5e3f2cef74f692d9af7f28ec219e4bc7131a4cca
supremgy <supremgy@icloud.com>
2024-07-15 03:18:10 +0900

    refactor: ActiveLink component 로직  수정

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -10,25 +10,12 @@ export default function ActiveLink({
   href: string;
 }) {
   const pathname = usePathname();
-  const team = pathname.split('/').pop();
   const isActive = pathname === href;
 
-  const teamColor = (team: string) => {
-    const teamColors: { [key: string]: string } = {
-      product: 'text-purple-600',
-      devops: 'text-blue-600',
-      success: 'text-amber-600',
-      laboratory: 'text-red-600',
-    };
-
-    return teamColors[team] || 'text-teal-600';
-  };
   return (
     <Link
       href={href}
-      className={`${
-        isActive ? `${team && teamColor(team)}` : 'text-black'
-      } capitalize`}
+      className={`${isActive ? 'text-teal-600' : 'text-black'} capitalize`}
     >
       {children}
     </Link>
