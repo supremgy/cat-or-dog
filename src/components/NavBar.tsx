2245b17a293f49fee62d1207a6f7017dcee9dad1
supremgy <supremgy@icloud.com>
2024-07-15 01:12:53 +0900

    feat: Navbar에 ActiveLink 컴포넌트 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,25 @@
-import Link from 'next/link';
+'use client';
 import React from 'react';
+import ActiveLink from './ActiveLink';
 
-export default function NavBar() {
+interface Team {
+  name: string;
+}
+interface Props {
+  teamList: Team[];
+}
+
+export default function NavBar({ teamList }: Props) {
   return (
-    <nav className='flex w-full justify-around gap-2 my-4 font-bold text-lg sm:text-2xl xs:text-base xxs:text-sm'>
-      <Link href={`/dashboard`}>InfoGrab</Link>
-      <Link href={`/dashboard/${'product'}`}>Product</Link>
-      <Link href={`/dashboard/${'devops'}`}>DevOps</Link>
-      <Link href={`/dashboard/${'success'}`}>Success</Link>
-      <Link href={`/dashboard/${'laboratory'}`}>Laboratory</Link>
+    <nav className=' flex my-4 justify-around gap-2 font-bold text-lg sm:text-2xl xs:text-base xxs:text-sm'>
+      <ActiveLink href={`/dashboard`}>InfoGrab</ActiveLink>
+
+      {teamList &&
+        teamList.map((item, i) => (
+          <ActiveLink key={i} href={`/dashboard/${item.name}`}>
+            {item.name}
+          </ActiveLink>
+        ))}
     </nav>
   );
 }
