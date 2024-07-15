2e8cf028ba751f76fff1fd81fd045d0e49e9b475
supremgy <supremgy@icloud.com>
2024-07-16 02:37:34 +0900

    refactor: base url 관련 오류 해결 중

--- src/service/team.ts
+++ src/service/team.ts
@@ -1,3 +1,4 @@
+import { headers } from 'next/headers';
 import { client } from './sanity';
 
 export async function fetchTeams() {
@@ -12,4 +13,25 @@ export async function fetchTeams() {
     console.error('Error fetching teams:', error);
     throw new Error('Failed to fetch teams');
   }
+}
+
+export async function getTeams() {
+  // const host = headers().get('host');
+  // const protocal = process?.env.NODE_ENV === 'development' ? 'http' : 'https';
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_PATH}/api/team`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch teams');
+  }
+
+  return await response.json();
 }
