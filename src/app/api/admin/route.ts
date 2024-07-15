ee8e79c05e63424cb8993b5993679f8aa74f8316
supremgy <supremgy@icloud.com>
2024-07-15 18:56:07 +0900

    feat: admin 로그인 api  생성

---
+++ src/app/api/admin/route.ts
@@ -0,0 +1,21 @@
+import { adminLogin } from '@/service/admin';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    const { team, nickname, password } = await req.json();
+
+    const loginResult = await adminLogin({ team, nickname, password });
+
+    if (loginResult) {
+      return NextResponse.json({ success: true });
+    } else {
+      return NextResponse.json(
+        { success: false, error: 'Unauthorized' },
+        { status: 401 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
