055547b0aa50972bed689f0e902f2b3afbf6e56a
supremgy <supremgy@icloud.com>
2024-07-19 15:10:47 +0900

    refactor: updataMember, React-Query Hook 생성

--- src/hook/useMember.ts
+++ src/hook/useMember.ts
@@ -1,20 +1,46 @@
 import { queryKeys } from '@/constants';
 import { useStore } from '@/store';
-import { getMembersByTeam } from '@/util/member';
-import { useQuery } from '@tanstack/react-query';
+import { getMembersByTeam, uploadMember } from '@/util/member';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const useGetMembersByTeam = () => {
   const team = useStore((state) => state.team);
-  const { data, isSuccess } = useQuery({
+  const setIsLoading = useStore((state) => state.setIsLoading);
+  const { data, isSuccess, isLoading, isFetching } = useQuery({
     queryKey: [queryKeys.MEMBER, team],
     queryFn: () => getMembersByTeam(team),
     staleTime: 0,
   });
-
+  useEffect(() => {
+    setIsLoading(isFetching);
+  }, [isFetching, setIsLoading]);
+  useEffect(() => {
+    setIsLoading(isLoading);
+  }, [isLoading, setIsLoading]);
   return isSuccess ? data : [];
 };
 
+const useUploadMember = () => {
+  const setIsLoading = useStore((state) => state.setIsLoading);
+  const team = useStore((state) => state.team);
+  const router = useRouter();
+  return useMutation({
+    mutationFn: uploadMember,
+    onSuccess: () => {
+      setIsLoading(false);
+      router.push(`/result?team=${team}`);
+    },
+  });
+};
+
 export const useMember = () => {
+  const setIsLoading = useStore((state) => state.setIsLoading);
   const members = useGetMembersByTeam();
-  return { members };
+  const uploadMember = useUploadMember();
+  useEffect(() => {
+    setIsLoading(uploadMember.isPending);
+  }, [setIsLoading, uploadMember.isPending]);
+  return { members, uploadMember };
 };
