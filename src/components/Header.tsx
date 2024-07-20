cb2f3aebaa844cf278b1df317a82173a12bd16b1
supremgy <supremgy@icloud.com>
2024-07-21 02:40:10 +0900

    refactor: /dashboard 경로 일때 isLoading false

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
 import InfoGrab from '../../public/infograb.svg';
 import { useStore } from '@/store';
@@ -10,6 +10,7 @@ export default function Header() {
   const pathname = usePathname();
   const router = useRouter();
   const setModalState = useStore((state) => state.setModalState);
+  const setIsLoading = useStore((state) => state.setIsLoading);
   const handleClick = () => {
     if (pathname === '/survey') {
       setModalState(true);
@@ -19,6 +20,9 @@ export default function Header() {
     }
     router.push('/');
   };
+  useEffect(() => {
+    if (pathname.startsWith('/dashboard')) setIsLoading(false);
+  }, [pathname, setIsLoading]);
   return (
     <header className='w-full h-10 flex justify-center mt-4'>
       <Image src={InfoGrab} alt='InfoGrab' height={30} onClick={handleClick} />
